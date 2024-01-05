3ce5cd983e9927ff8544affc829bc669abdcf8e4
jerome <bernarjerome59@gmail.com>
2024-01-05 10:58:14 +0100

    Change comment

--- lockboxes/0-lockboxes.py
+++ lockboxes/0-lockboxes.py
@@ -13,32 +13,37 @@ def canUnlockAll(boxes: List[List[int]]) -> bool:
     """
     Determines if all the boxes can be opened.
 
-    Args:
+    Args :
         boxes (List[List[int]]):
          A list of lists where each inner list represents a box
            and contains keys to other boxes.
 
-    Returns:
+    Returns :
         bool: True if all boxes can be opened, False otherwise.
     """
+
+    """Verify if all box are empty or null"""
     if not boxes or len(boxes) == 0:
         return False
 
-    # Initialize unlocked boxes (the first box is unlocked)
+    """Initialize unlocked boxes (the first box is unlocked)"""
     unlocked_boxes = [False] * len(boxes)
     unlocked_boxes[0] = True
 
-    # Initialize available keys with the keys from the first box
+    """Initialize available keys with the keys from the first box"""
     keys = boxes[0]
 
-    # Traverse available keys
+    """Traverse available keys"""
     while keys:
-        current_key = keys.pop()  # Take a key from the list
+        """Take a key from the list"""
+        current_key = keys.pop()
 
-        # Check if the key opens a new unopened box
+        """Check if the key opens a new unopened box"""
         if current_key < len(boxes) and not unlocked_boxes[current_key]:
-            unlocked_boxes[current_key] = True  # Unlock the corresponding box
-            keys.extend(boxes[current_key])  # Add newly found keys
+            """Unlock the corresponding box"""
+            unlocked_boxes[current_key] = True
+            """Add newly found keys"""
+            keys.extend(boxes[current_key])
 
-    # Check if all boxes are unlocked
+    """Check if all boxes are unlocked"""
     return all(unlocked_boxes)
