979a446f50f0eb108fd7d0bdc8fcff4f69776fc2
xdJidx <bernardjerome59@gmail.com>
2024-02-09 09:11:56 +0100

    Linked list palindrome

---
+++ linked_list_palindrome/0-is_palindrome.c
@@ -0,0 +1,63 @@
+#include "lists.h"
+#include <stddef.h>
+
+/**
+ * reverse_list - reverses a linked list
+ * @head: pointer to the head of the list
+ * Return: pointer to the new head of the reversed list
+ */
+
+
+listint_t *reverse_list(listint_t **head)
+{
+    listint_t *prev = NULL;
+    listint_t *current = *head;
+    listint_t *next = NULL;
+
+    while (current != NULL)
+    {
+        next = current->next;
+        current->next = prev;
+        prev = current;
+        current = next;
+    }
+
+    *head = prev;
+    return *head;
+}
+
+
+/**
+ * is_palindrome - checks if a singly linked list is a palindrome
+ * @head: pointer to the head of the list
+ * Return: 1 if the list is a palindrome, 0 otherwise
+ */
+int is_palindrome(listint_t **head)
+{
+    if (*head == NULL || (*head)->next == NULL)
+        return 1;
+
+    listint_t *slow = *head;
+    listint_t *fast = *head;
+
+    while (fast != NULL && fast->next != NULL)
+    {
+        slow = slow->next;
+        fast = fast->next->next;
+    }
+
+    // Reverse the second half of the list
+    slow = reverse_list(&slow);
+
+    // Compare the reversed second half with the first half
+    while (slow != NULL)
+    {
+        if ((*head)->n != slow->n)
+            return 0;
+
+        *head = (*head)->next;
+        slow = slow->next;
+    }
+
+    return 1;
+}
