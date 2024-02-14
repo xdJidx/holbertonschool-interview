36ff985e024f7a32ef7ddf158ed137f909eb4755
xdJidx <bernardjerome59@gmail.com>
2024-02-14 14:22:21 +0100

    Change for pycodestyle and documented module

--- log_parsing/0-stats.py
+++ log_parsing/0-stats.py
@@ -1,16 +1,18 @@
 #!/usr/bin/python3
+""" Log parsing """
 import sys
 import signal
 
 
 # Variables to store metrics
 total_file_size = 0
-status_code_counts = {200: 0, 301: 0, 400: 0, 401: 0, 403: 0, 404: 0, 405: 0, 500: 0}
+status_code_counts = {200: 0, 301: 0, 400: 0, 401: 0, 403: 0,
+                      404: 0, 405: 0, 500: 0}
 
 try:
     for i, line in enumerate(sys.stdin, start=1):
         parts = line.strip().split()
-        
+
         # Check if the line follows the specified format
         if len(parts) == 10 and parts[5] == '"GET' and parts[7].isdigit():
             file_size = int(parts[7])
