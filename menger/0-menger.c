e75c00004049fb4c020146e1ecbb46532e9f0048
xdJidx <bernardjerome59@gmail.com>
2024-03-31 18:23:01 +0200

    Correction betty

--- menger/0-menger.c
+++ menger/0-menger.c
@@ -2,22 +2,27 @@
 #include <math.h>
 
 /**
- * @brief Dessine un caractère "#" ou un espace en fonction des coordonnées et de la taille spécifiées.
+ * draw_char - Dessine un caractère à l'intérieur d'une grille de
+ * coordonnées et de la taille spécifiées.
  *
- * Cette fonction affiche un caractère "#" si les coordonnées (x, y) sont à l'intérieur
- * de la grille de taille size, sinon elle affiche un espace.
+ * Cette fonction affiche un caractère "#" si les coordonnées (x, y)
+ * sont à l'intérieur de la grille de taille size, sinon elle affiche
+ * un espace.
  *
- * @param size Taille de la grille.
- * @param x Coordonnée horizontale.
- * @param y Coordonnée verticale.
+ * @size: Taille de la grille.
+ * @x: Coordonnée horizontale.
+ * @y: Coordonnée verticale.
  */
 void draw_char(int size, int x, int y)
 {
-    if ((x >= size) || (y >= size) || (x < 0) || (y < 0)) {
-        printf(" ");
-    } else {
-        printf("#");
-    }
+	if ((x >= size) || (y >= size) || (x < 0) || (y < 0))
+	{
+		printf(" ");
+	}
+	else
+	{
+		printf("#");
+	}
 }
 
 /**
@@ -26,29 +31,35 @@ void draw_char(int size, int x, int y)
  */
 void menger(int level)
 {
-    int size, x, y, div_x, div_y;
+	int size, x, y, div_x, div_y;
 
-    if (level < 0) return;
+	if (level < 0)
+		return;
 
-    size = (int)pow(3, level);
+	size = (int)pow(3, level);
 
-    for (y = 0; y < size; y++) {
-        for (x = 0; x < size; x++) {
-            div_x = x;
-            div_y = y;
+	for (y = 0; y < size; y++)
+	{
+		for (x = 0; x < size; x++)
+		{
+			div_x = x;
+			div_y = y;
 
-            while (div_x > 0 && div_y > 0) {
-                if (div_x % 3 == 1 && div_y % 3 == 1) {
-                    printf(" ");
-                    break;
-                }
-                div_x /= 3;
-                div_y /= 3;
-            }
-            if (div_x == 0 || div_y == 0) {
-                draw_char(size, x, y);
-            }
-        }
-        printf("\n");
-    }
+			while (div_x > 0 && div_y > 0)
+			{
+				if (div_x % 3 == 1 && div_y % 3 == 1)
+				{
+					printf(" ");
+					break;
+				}
+				div_x /= 3;
+				div_y /= 3;
+			}
+			if (div_x == 0 || div_y == 0)
+			{
+				draw_char(size, x, y);
+			}
+		}
+		printf("\n");
+	}
 }
