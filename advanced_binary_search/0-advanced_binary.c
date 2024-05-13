c0ae029ff212c7038cfb779bd994cf4c5df4bd8d
xdJidx <bernardjerome59@gmail.com>
2024-05-13 13:35:15 +0200

    Correction standard C99

--- advanced_binary_search/0-advanced_binary.c
+++ advanced_binary_search/0-advanced_binary.c
@@ -11,18 +11,19 @@
  */
 int advanced_binary(int *array, size_t size, int value)
 {
-	if (array == NULL || size == 0)
-		return (-1);
-
 	size_t low = 0;
 	size_t high = size - 1;
+	size_t i;
+
+	if (array == NULL || size == 0)
+		return (-1);
 
 	while (low <= high)
 	{
 		size_t mid = (low + high) / 2;
 
 		printf("Searching in array: ");
-		for (size_t i = low; i <= high; i++)
+		for (i = low; i <= high; i++)
 		{
 			printf("%d", array[i]);
 			if (i < high)
