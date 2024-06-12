33c7c1c99e71881c75dbb105f66b65cb8cdf0b33
xdJidx <bernardjerome59@gmail.com>
2024-06-12 10:39:01 +0200

    Correction pycodestyle

--- rotate_2d_matrix/0-rotate_2d_matrix.py
+++ rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -1,15 +1,25 @@
 #!/usr/bin/python3
 """"Rotate a 2D matrix by 90 degrees clockwise."""
 
+
 def rotate_2d_matrix(matrix):
+    """
+    Rotate a 2D matrix by 90 degrees clockwise.
+
+    Args:
+        matrix: A 2D matrix.
+
+    Returns:
+        None.
+    """
     n = len(matrix)
-    
+
     # Transpose the matrix
     for i in range(n):
         for j in range(i + 1, n):
             # Swap elements matrix[i][j] and matrix[j][i]
             matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
-    
+
     # Reverse each row
     for i in range(n):
         matrix[i].reverse()
