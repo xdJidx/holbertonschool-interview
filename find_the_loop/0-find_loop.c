5bbe6db537be0734d8fcaf685f809b32a21fba9f
xdJidx <bernardjerome59@gmail.com>
2024-06-28 09:26:05 +0200

    Re correction betty

--- find_the_loop/0-find_loop.c
+++ find_the_loop/0-find_loop.c
@@ -4,7 +4,7 @@
  * find_listint_loop - finds the loop in a linked list.
  * @head: pointer to the head of the linked list.
  *
- * Return: The address of the node where the loop starts, 
+ * Return: The address of the node where the loop starts,
  * or NULL if there is no loop.
  */
 listint_t *find_listint_loop(listint_t *head)
