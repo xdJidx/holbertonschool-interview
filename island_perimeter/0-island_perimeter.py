069a04af933f086ac3fafe3cab407809f6ea7d3b
xdJidx <bernardjerome59@gmail.com>
2024-08-02 09:16:59 +0200

    Correction Function documented

--- island_perimeter/0-island_perimeter.py
+++ island_perimeter/0-island_perimeter.py
@@ -1,8 +1,18 @@
 #!/usr/bin/python3
-"""#!/usr/bin/python3"""
+""" Island Perimeter """
 
 
 def island_perimeter(grid):
+    """
+    Function that returns the perimeter of the island described in grid
+
+    Arguments:
+    grid: a list of list of integers
+
+    Returns:
+    The perimeter of the island described in grid
+    """
+
     rows = len(grid)
     cols = len(grid[0])
     perimeter = 0
