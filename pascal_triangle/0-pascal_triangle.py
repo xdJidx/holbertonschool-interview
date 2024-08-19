4d79ca8506092b10e80aa8c8d6435b6551e1ae4f
xdJidx <bernardjerome59@gmail.com>
2024-08-19 15:24:03 +0200

    Change code

--- pascal_triangle/0-pascal_triangle.py
+++ pascal_triangle/0-pascal_triangle.py
@@ -1,30 +1,19 @@
 #!/usr/bin/python3
-""" Triangle de Pascal """
+"""
+Returns list representing the Pascal's triangle of n
+"""
 
 
 def pascal_triangle(n):
+    """returns empty list if n <= 0"""
+    pascal_list = []
     if n <= 0:
-        return []
-
-    triangle = []
-
+        return pascal_list
     for i in range(n):
-        # Commencer chaque ligne avec un 1
-        row = [1] * (i + 1)
-        for j in range(1, i):
-            # Chaque valeur est la somme des deux valeurs directement au-dessus
-            row[j] = triangle[i - 1][j - 1] + triangle[i - 1][j]
-        triangle.append(row)
-
-    return triangle
-
-
-if __name__ == "__main__":
-    def print_triangle(triangle):
-        """
-        Print the triangle
-        """
-        for row in triangle:
-            print("[{}]".format(",".join([str(x) for x in row])))
-
-    print_triangle(pascal_triangle(5))
+        row = [1]
+        if pascal_list:
+            last_row = pascal_list[-1]
+            row.extend([sum(pair) for pair in zip(last_row, last_row[1:])])
+            row.append(1)
+        pascal_list.append(row)
+    return (pascal_list)
