f72b4e7b798efebb55f753d8ff2978ffee8746d4
xdJidx <bernardjerome59@gmail.com>
2024-08-19 15:43:13 +0200

    Test new code

--- pascal_triangle/0-pascal_triangle.py
+++ pascal_triangle/0-pascal_triangle.py
@@ -1,28 +1,24 @@
 #!/usr/bin/python3
-"""
-Module pour générer le Triangle de Pascal.
-"""
+"""Pascal's Triangle, task 0. Pascal's Triangle"""
 
 
 def pascal_triangle(n):
-    """
-    Retourne une liste représentant le triangle de Pascal de taille n.
+    """Returns a list of lists of integers representing
+    the Pascal's triangle of `n`.
 
     Args:
-        n (int): Le nombre de lignes du triangle de Pascal à générer.
+        n (int): base number for calculating triangle
 
-    Returns:
-        List[List[int]]: Une liste de listes d'entiers représentant
-        le triangle de Pascal.
     """
-    pascal_list = []
+    triangle = []
     if n <= 0:
-        return pascal_list
-    for i in range(n):
-        row = [1]
-        if pascal_list:
-            last_row = pascal_list[-1]
-            row.extend([sum(pair) for pair in zip(last_row, last_row[1:])])
-            row.append(1)
-        pascal_list.append(row)
-    return pascal_list
+        return triangle
+    for row in range(n):
+        new_row = []
+        triangle.append(new_row)
+        new_row.append(1)
+        for col in range(1, row):
+            new_row.append(triangle[row - 1][col - 1] + triangle[row - 1][col])
+        if row > 0:
+            new_row.append(1)
+    return triangle
