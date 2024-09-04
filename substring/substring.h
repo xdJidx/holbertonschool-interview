986e1f22bab18378cf219f3eb610a04a8aaa1d79
xdJidx <bernardjerome59@gmail.com>
2024-09-04 14:24:47 +0200

    Betty correction

--- substring/substring.h
+++ substring/substring.h
@@ -7,12 +7,15 @@
 /*
  * Prototype de la fonction find_substring.
  * s : chaîne de caractères à analyser
- * words : tableau de mots, dont chaque sous-chaîne valide doit être une concaténation
+ * words : tableau de mots, dont chaque sous-chaîne valide doit être une
+ * concaténation
  * nb_words : nombre d'éléments dans le tableau words
  * n : adresse où stocker le nombre d'éléments dans le tableau retourné
  *
- * La fonction retourne un tableau alloué dynamiquement, stockant chaque index dans s
- * où une sous-chaîne valide a été trouvée. Si aucune solution n'est trouvée, elle retourne NULL.
+ * La fonction retourne un tableau alloué dynamiquement, stockant chaque
+ * index dans s
+ * où une sous-chaîne valide a été trouvée. Si aucune solution n'est trouvée,
+ * elle retourne NULL.
  */
 int	*find_substring(char const *s, char const **words, int nb_words, int *n);
 
